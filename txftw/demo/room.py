dd7213e7372938c8663a4074b551750d6b3213aa
Matt Haggard <haggardii@gmail.com>
2013-08-20 11:42:26 -0600

    Names are part of Room now

--- txftw/demo/room.py
+++ txftw/demo/room.py
@@ -2,6 +2,10 @@ class AlreadyInTheRoom(Exception): pass
 class NotInRoom(Exception): pass
 
 
+from uuid import uuid4
+
+
+
 class Room(object):
     """
     I am a room where things can gather and pass around messages.
@@ -9,36 +13,52 @@ class Room(object):
 
 
     def __init__(self):
-        self._contents = []
+        self._contents = {}
 
 
-    def enter(self, thing):
+    def enter(self, name, thing):
         """
-        C{thing} is entering this room.
+        C{thing} is entering this room under the alias C{name}.
+
+        @param name: Name of the thing entering this room.  This must be unique
+            within this room.
+        @param thing: Some object with C{setRoom(room)} and
+            C{messageReceived(message)} methods.
         """
-        if thing in self._contents:
-            raise AlreadyInTheRoom(thing)
-        thing.room = self
-        self._contents.append(thing)
+        if name in self._contents:
+            raise AlreadyInTheRoom(name)
+        thing.setRoom(self)
+        self._contents[name] = thing
         self.broadcast({
             'event': 'enter',
-            'who': thing.identity()
+            'who': name,
         })
 
 
-    def leave(self, thing):
+    def leave(self, name):
         """
-        C{thing} is leaving this room.
+        The thing named C{name} is leaving this room.
         """
         try:
-            self._contents.remove(thing)
-        except ValueError:
-            raise NotInRoom(thing)
-        thing.room = None
+            thing = self._contents.pop(name)
+        except KeyError:
+            raise NotInRoom(name)
+        thing.setRoom(None)
         self.broadcast({
             'event': 'leave',
-            'who': thing.identity(),
+            'who': name,
         })
+
+
+    def kick(self, name):
+        """
+        The thing named C{name} is being kicked out of the room.
+        """
+        self.broadcast({
+            'event': 'kick',
+            'kicked': name,
+        })
+        self.leave(name)
 
 
     def contents(self):
@@ -52,5 +72,47 @@ class Room(object):
         """
         Send a message to all things in this room.
         """
-        for thing in self._contents:
+        for name,thing in self._contents.items():
             thing.messageReceived(message)
+
+
+
+class Building(object):
+    """
+    I am a building full of rooms.
+    """
+
+
+    def __init__(self):
+        self._rooms = {}
+
+
+    def createRoom(self):
+        """
+        Provision a new room.
+
+        @return: A C{key} to be used when calling L{getRoom} and L{destroyRoom}.
+        """
+        key = str(uuid4())
+        self._rooms[key] = Room()
+        return key
+
+
+    def getRoom(self, key):
+        """
+        Get the L{Room} associated with a C{key}.
+        """
+        return self._rooms[key]
+
+
+    def destroyRoom(self, key):
+        """
+        Destroy a room (kicking everyone out of it)
+        """
+        room = self.getRoom(key)
+        del self._rooms[key]
+        for thing in room.contents():
+            room.kick(thing)
+
+
+
--- txftw/demo/test/test_room.py
+++ txftw/demo/test/test_room.py
@@ -1,7 +1,7 @@
 from twisted.trial.unittest import TestCase
-from mock import MagicMock
+from mock import MagicMock, create_autospec
 
-from txftw.demo.room import Room, NotInRoom
+from txftw.demo.room import Room, Building, NotInRoom
 
 
 
@@ -15,9 +15,9 @@ class RoomTest(TestCase):
         room = Room()
         thing = MagicMock()
 
-        room.enter(thing)
-        self.assertEqual(thing.room, room, "Should set room")
-        self.assertEqual(room.contents(), [thing])
+        room.enter('name', thing)
+        thing.setRoom.assert_called_once_with(room)
+        self.assertEqual(room.contents(), {'name': thing})
 
 
     def test_enter_twice(self):
@@ -27,8 +27,8 @@ class RoomTest(TestCase):
         room = Room()
         thing = MagicMock()
         
-        room.enter(thing)
-        self.assertRaises(Exception, room.enter, thing)
+        room.enter('name', thing)
+        self.assertRaises(Exception, room.enter, 'name', thing)
 
 
     def test_leave(self):
@@ -38,10 +38,12 @@ class RoomTest(TestCase):
         room = Room()
         thing = MagicMock()
 
-        room.enter(thing)
-        room.leave(thing)
-        self.assertEqual(thing.room, None)
-        self.assertEqual(room.contents(), [])
+        room.enter('thing', thing)
+        thing.setRoom.reset_mock()
+
+        room.leave('thing')
+        thing.setRoom.assert_called_once_with(None)
+        self.assertEqual(room.contents(), {})
 
 
     def test_leave_beforeEntering(self):
@@ -50,9 +52,9 @@ class RoomTest(TestCase):
         """
         room = Room()
         thing = MagicMock()
-        thing.room = 'foo'
-        self.assertRaises(NotInRoom, room.leave, thing)
-        self.assertEqual(thing.room, 'foo', "Should not have changed the room")
+        self.assertRaises(NotInRoom, room.leave, 'thing')
+        self.assertEqual(thing.setRoom.call_count, 0,
+                         "Should not have changed the room")
 
 
     def test_broadcast(self):
@@ -62,7 +64,7 @@ class RoomTest(TestCase):
         room = Room()
         thing = MagicMock()
 
-        room.enter(thing)
+        room.enter('name', thing)
         room.broadcast('message')
         thing.messageReceived.assert_any_call('message')
 
@@ -73,21 +75,86 @@ class RoomTest(TestCase):
         """
         room = Room()
         thing1 = MagicMock()
-        room.enter(thing1)
+        room.enter('t1', thing1)
 
         thing2 = MagicMock()
-        thing2.identity.return_value = {'name': 'my name'}
         
-        room.enter(thing2)
+        room.enter('t2', thing2)
         thing1.messageReceived.assert_any_call({
             'event': 'enter',
-            'who': {'name': 'my name'},
+            'who': 't2',
         })
 
-        room.leave(thing2)
+        room.leave('t2')
         thing1.messageReceived.assert_any_call({
             'event': 'leave',
-            'who': {'name': 'my name'},
+            'who': 't2',
         })
+
+
+    def test_kick(self):
+        """
+        You can kick things out of a room
+        """
+        room = Room()
+        thing1 = MagicMock()
+        thing2 = MagicMock()
+
+        room.enter('t1', thing1)
+        thing1.setRoom.reset_mock()
+        room.enter('t2', thing2)
+
+        room.kick('t1')
+        thing2.messageReceived.assert_any_call({
+            'event': 'kick',
+            'kicked': 't1',
+        })
+        thing1.setRoom.assert_called_once_with(None)
+        self.assertEqual(room.contents(), {'t2': thing2})
+
+
+
+class BuildingTest(TestCase):
+
+
+    def test_create(self):
+        """
+        You can make new rooms
+        """
+        building = Building()
+        key = building.createRoom()
+        self.assertTrue(isinstance(key, str))
+
+        room = building.getRoom(key)
+        self.assertTrue(isinstance(room, Room))
+        room2 = building.getRoom(key)
+        self.assertEqual(room, room2, "Same key should return same room")
+
+
+    def test_createRoom_uniqueKey(self):
+        building = Building()
+        k1 = building.createRoom()
+        k2 = building.createRoom()
+        self.assertNotEqual(k1, k2)
+
+
+    def test_destroy(self):
+        """
+        You can destroy rooms (kicking everyone in them)
+        """
+        building = Building()
+        key = building.createRoom()
+        room = building.getRoom(key)
+        room.kick = create_autospec(room.kick)
+
+        thing1 = MagicMock()
+        thing2 = MagicMock()
+        room.enter('t1', thing1)
+        room.enter('t2', thing2)
+
+        building.destroyRoom(key)
+        room.kick.assert_any_call('t1')
+        room.kick.assert_any_call('t2')
+        self.assertRaises(KeyError, building.getRoom, key)
 
 
